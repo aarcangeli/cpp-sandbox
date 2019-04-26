21cef12bd09e8a8e5859c3b5a9f4672ab528d9c7
Alessandro Arcangeli <alessandroarcangeli.rm@gmail.com>
2019-04-26 11:37:29 +0200

    pthread

--- src/threads/pthread/monitor.cpp
+++ src/threads/pthread/monitor.cpp
@@ -2,8 +2,42 @@
 #include <unistd.h>
 #include "pthread.h"
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+/*
+ * In this example we are testing the a condition variable (pthread_cond_t myCond)
+ *
+ * 'predicate' and 'mustExit' is shared variables protected by 'myMutex',
+ * so you must ensure that 'myMutex' is locked before using it (both
+ * read and update)
+ *
+ * There are two threads: a controller and a monitor.
+ *
+ *   - The controller set the 'predicate' flag when it require an action from the 'monitor' thread
+ *   - The monitor sleeps when the 'predicate' flag is unset and does something if set
+ *
+ * Typical output:
+ *   - [monitor] Started
+ *   - [monitor] Sleeping
+ *   - [controller] Signaling monitor
+ *   - [monitor] Awakened
+ *   - [monitor] Sleeping
+ *   - [controller] Signaling monitor
+ *   - [monitor] Awakened
+ *   - [monitor] Sleeping
+ *   - [controller] Signaling monitor
+ *   - [monitor] Awakened
+ *   - [monitor] Sleeping
+ *   - [controller] Signaling monitor
+ *   - [monitor] Awakened
+ *   - [monitor] Sleeping
+ *   - [controller] Signaling monitor
+ *   - [monitor] Awakened
+ *   - [monitor] Sleeping
+ *   - [controller] Signaling exit
+ *   - [monitor] Stopped
+ */
+
+pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t myCond = PTHREAD_COND_INITIALIZER;
 bool predicate = false;
 bool mustExit = false;
 
@@ -11,19 +45,19 @@ void *monitorThread(void *unused) {
     printf("  - [monitor] Started\n");
 
     while (true) {
-        pthread_mutex_lock(&mutex);
-        predicate = false;
+        pthread_mutex_lock(&myMutex);
         printf("  - [monitor] Sleeping\n");
         while (!predicate) {
-            pthread_cond_wait(&cond, &mutex);
+            pthread_cond_wait(&myCond, &myMutex);
         }
+        predicate = false;
         if (mustExit) {
-            pthread_mutex_unlock(&mutex);
+            pthread_mutex_unlock(&myMutex);
             break;
         }
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&myMutex);
 
-        printf("  - [monitor] Awaked\n");
+        printf("  - [monitor] Awakened\n");
         // working
     }
 
@@ -32,33 +66,33 @@ void *monitorThread(void *unused) {
 }
 
 void signalMonitor() {
-    pthread_mutex_lock(&mutex);
-    pthread_cond_signal(&cond);
+    pthread_mutex_lock(&myMutex);
+    pthread_cond_signal(&myCond);
     predicate = true;
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&myMutex);
 }
 
 void signalExit() {
-    pthread_mutex_lock(&mutex);
-    pthread_cond_signal(&cond);
+    pthread_mutex_lock(&myMutex);
+    pthread_cond_signal(&myCond);
     predicate = true;
     mustExit = true;
-    pthread_mutex_unlock(&mutex);
+    pthread_mutex_unlock(&myMutex);
 }
 
 int main() {
     pthread_t threadSignal;
 
-    pthread_create(&threadSignal, 0, &monitorThread, nullptr);
+    pthread_create(&threadSignal, nullptr, &monitorThread, nullptr);
     sleep(3);
 
     for (int j = 0; j < 5; ++j) {
-        printf("  - Signaling monitor\n");
+        printf("  - [controller] Signaling monitor\n");
         signalMonitor();
         sleep(3);
     }
 
-    printf("  - Signaling exit\n");
+    printf("  - [controller] Signaling exit\n");
     signalExit();
 
     pthread_join(threadSignal, nullptr);
--- src/threads/pthread/mutex.cpp
+++ src/threads/pthread/mutex.cpp
@@ -2,32 +2,51 @@
 #include <unistd.h>
 #include "pthread.h"
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-bool signalized = false;
+/*
+ * In this example we are creating 5 different thread that
+ * try to lock the same mutex for a second.
+ *
+ * This test shows that a mutex is locked in an undefined order.
+ *
+ * Typical output:
+ *   - thread 0 started
+ *   - thread 0 stopped
+ *   - thread 2 started
+ *   - thread 2 stopped
+ *   - thread 3 started
+ *   - thread 3 stopped
+ *   - thread 1 started
+ *   - thread 1 stopped
+ *   - thread 4 started
+ *   - thread 4 stopped
+ *   - END
+ */
 
-void *demoSync(void *id) {
-    pthread_mutex_lock(&mutex);
+pthread_mutex_t myMutex = PTHREAD_MUTEX_INITIALIZER;
 
-    printf("  - [%zu] started\n", (size_t) id);
+void *demoSync(void *_id) {
+    size_t id = (size_t) _id;
+    pthread_mutex_lock(&myMutex);
+    printf("  - thread %zu started\n", id);
+
     sleep(1);
-    printf("  - [%zu] stopped\n", (size_t) id);
 
-    pthread_mutex_unlock(&mutex);
+    printf("  - thread %zu stopped\n", id);
+    pthread_mutex_unlock(&myMutex);
     return nullptr;
 }
 
 int main() {
-    const int totalThreads = 20;
+    const int totalThreads = 5;
     pthread_t thread[totalThreads];
 
     for (size_t i = 0; i < totalThreads; ++i) {
-        pthread_create(&thread[i], 0, &demoSync, (void *) i);
-        usleep(10);
+        pthread_create(&thread[i], nullptr, &demoSync, (void *) i);
     }
 
-    for (int i = 0; i < totalThreads; ++i) {
-        pthread_join(thread[i], nullptr);
+    for (pthread_t i : thread) {
+        pthread_join(i, nullptr);
     }
 
-    printf(" - END\n");
+    printf("  - END\n");
 }
